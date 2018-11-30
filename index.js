96d35ec86eb5ec7fe2e1ea5e60dd5589a549f68f
Emil Zhai <root@derzh.com>
2018-11-30 18:19:36 +0800

    fix: add index file for import problem

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @Author: Emil Zhai (root@derzh.com)
+ * @Date:   2018-11-30 18:16:26
+ * @Last Modified by:   Emil Zhai (root@derzh.com)
+ * @Last Modified time: 2018-11-30 18:17:14
+ */
+import photoswipe from './dist/vue-photoswipe.common';
+
+export default photoswipe;
