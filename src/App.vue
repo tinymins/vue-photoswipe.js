78a4379352944f44065972f1cceb3a50db050bc0
Emil Zhai <root@derzh.com>
2019-04-03 11:11:34 +0800

    feat: modify test case

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,22 @@
   <div style="text-align: center;">
     <div style="padding: 15px 0;">PhotoSwipe for Vue.js</div>
     <img v-photoswipe src="https://www.google.com.hk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+    <img v-photoswipe:group1 src="https://www.google.com.hk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
+    <img v-photoswipe:group1 src="https://www.google.com.hk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png">
   </div>
 </template>
 
 <script>
 export default {
   mounted() {
-    window.a = this;
     this.$photoswipe.listen('beforeChange', this.beforeChange);
   },
   beforeDestroy() {
     this.$photoswipe.unlisten('beforeChange', this.beforeChange);
   },
   methods: {
-    beforeChange() {
-      console.log('beforeChange'); // eslint-disable-line
+    beforeChange(...args) {
+      console.log('beforeChange', ...args); // eslint-disable-line
     },
   },
 };
